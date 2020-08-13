c26e1376a5c41cba7f9f33b849d0379e111af062
himanshuMaheshwari2311 <m.himanshu2311@gmail.com>
2020-08-13 23:24:39 +0530

    Added basic search

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from .endpoints import data
+from .endpoints import data, search
 
 api_router = APIRouter()
 api_router.include_router(data.router)
+api_router.include_router(search.router)
--- frontend/src/components/sidenav/Sidenav.tsx
+++ frontend/src/components/sidenav/Sidenav.tsx
@@ -9,12 +9,14 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
-import React from 'react';
+import React, { useState } from 'react';
 import { Blogs } from '../blogs/Blogs';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
 import { categories } from '../../constant/blog-type.const';
+import { Blog } from '../../model/Blog';
+import searchBlog from '../../service/SearchBlog';
 
 const drawerWidth = 240;
 
@@ -81,10 +83,23 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const Sidenav: React.FC = () => {
     const classes = useStyles();
-    const [tag, setTag] = React.useState('General');
+    const [tag, setTag] = useState('General');
+    const [search, setSearch] = useState('');
+    const [blogs, setBlogs] = useState<Blog[]>([]);
 
     const updateTag = (tag: string) => {
         setTag(tag);
+    }
+
+    const setSearchText = (event: any) => {
+        setSearch(event.target.value);
+    }
+
+    const searchBlogHandle = () => {
+        searchBlog(search).then(blogs => {
+            setBlogs(blogs.data);
+        });
+        console.log(blogs);
     }
 
     return (
@@ -100,8 +115,9 @@ const Sidenav: React.FC = () => {
                         className={classes.input}
                         placeholder="Search"
                         inputProps={{ 'aria-label': 'search' }}
+                        onChange={setSearchText}
                     />
-                    <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                    <IconButton type="submit" className={classes.iconButton} aria-label="search" onClick={searchBlogHandle}>
                         <SearchIcon />
                     </IconButton>
                 </Toolbar>
